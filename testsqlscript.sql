78e78196bf2ff0e3471f29e42dc4ba4b730eb030
Papun Mohanty <papunmohanty31@gmail.com>
2019-02-08 15:24:47 +0530

    Update testsqlscript.sql

--- testsqlscript.sql
+++ testsqlscript.sql
@@ -1,1 +1,36 @@
-select top 1 * from MPR.tblListing_Event
+SELECT
+[BuilderDimKey]
+,[BuilderID]
+,[IsDeleted]
+,[DateIsLive]
+,[BDXBuilderID]
+,[BuilderNumber]
+,[BrandName]
+,[ReportingName]
+,[DefaultLeadsEmail]
+,[DefaultLeadsEmailLeadsPerMessage]
+,[CopyLeadsEmail]
+,[CopyLeadsEmailLeadsPerMessage]
+,[BuilderWebsite]
+,[BrandLogo]
+,[CorpID]
+,[CorpBuilderNumber]
+,[CorpState]
+,[CorpName]
+,[CorpReportingEmail]
+,[SendToCorpOnly]
+,[AMBuilderRecordStatus]
+,CONVERT(VARCHAR(128),[EffectiveFrom]) [EffectiveFrom]
+,CONVERT(VARCHAR(128),[EffectiveTo]) [EffectiveTo]
+,CONVERT(VARCHAR(128),[CreatedDate]) [CreatedDate]
+,[CreatedBy]
+,CONVERT(VARCHAR(128),[ModifiedDate]) [ModifiedDate]
+,[Modifiedby]
+FROM [MoveDM].[EDW].[tblBuilderDim] WITH(NOLOCK)
+
+  WHERE 
+	(
+ (EffectiveFrom >= (convert(date,GETDATE()-convert(int,'$$SourceLookBackFromDays'))) and EffectiveFrom < (convert(date,GETDATE()-convert(int,'$$SourceLookBackToDays'))))
+or
+((EffectiveTo >= (convert(date,GETDATE()-convert(int,'$$SourceLookBackFromDays'))) and EffectiveTo <> '9999-12-31 23:59:59.9999999') and EffectiveTo < CONVERT(date,GETDATE()-convert(int,'$$SourceLookBackToDays')))
+)
